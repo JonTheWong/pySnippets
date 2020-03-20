6e5d8d5e6f350f914760f40625cc7735e758b591
Jon Wong <me@jonthewong.com>
2020-03-19 21:22:01 -0400

    Initial read from CSV snippet
    Signed-off-by: Jon Wong <me@jonthewong.com>

---
+++ code/readcsv.py
@@ -0,0 +1,19 @@
+import csv
+
+
+# Documentation
+# https://docs.python.org/3.7/library/csv.html
+
+
+def readCSV():
+    '''Read file, return the content as a list'''
+    file = '../data/file0.csv'
+    with open(file, newline='') as data:
+        data = csv.reader(data, delimiter=',', quotechar='|')
+        rowofdata = []
+        for row in data:
+            rowofdata.append(row)
+    return rowofdata
+
+
+print(readCSV())   # expected output [['file0,line0'], ['file0,line1'], ['file0,line2'], ['file0,line3'], ['file0,line4'], ['file0,line5']]
