b9200e2144433b5ec009aab17c27d62a7bff816d
Jon Wong <me@jonthewong.com>
2020-03-20 01:38:58 -0400

    Added file paths
    Remembered i could remove the hard coded paths for the functions and pass them via the main app code.
    
    Signed-off-by: Jon Wong <me@jonthewong.com>

--- importing_custom_modules.py
+++ importing_custom_modules.py
@@ -1,6 +1,9 @@
 from code import readfile, readcsv
-# Based on https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder/20753073#20753073
+
+
+# Documentation - https://docs.python.org/3.7/library/importlib.html
+# Based on https://stackoverflow.com/a/20749411/8949196, https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder/20753073#20753073
 # Atom + pyflakes doesnt like * will have to define them manually; but python doesnt complain.
 
-print(readfile.readfile())
-print(readcsv.readcsv())
+print(readfile.readfile('data/file0.txt'))
+print(readcsv.readcsv('data/file0.csv'))
