ccdd12c3f0cc83135279c61a49f85758987de1c6
Jon Wong <me@jonthewong.com>
2020-07-24 12:26:08 -0400

    added encoding
    Signed-off-by: Jon Wong <me@jonthewong.com>

--- code/readcsv.py
+++ code/readcsv.py
@@ -3,6 +3,7 @@ import csv
 
 # Documentation
 # https://docs.python.org/3.7/library/csv.html
+# csv.reader already splits the list data so daae[0] works out of the box.
 
 
 def readcsv(filepath):
@@ -10,7 +11,7 @@ def readcsv(filepath):
     Read file in filepath, return all rows as a list type.
     Call function: ```readcsv('data/file0.csv')```
     '''
-    with open(filepath, newline='') as data:
+    with open(filepath, newline='', encoding='utf-8') as data:
         data = csv.reader(data, delimiter=',', quotechar='|')
         rowofdata = []
         for row in data:
@@ -18,4 +19,4 @@ def readcsv(filepath):
     return rowofdata
 
 
-# print(readcsv())   # expected output [['file0,line0'], ['file0,line1'], ['file0,line2'], ['file0,line3'], ['file0,line4'], ['file0,line5']]
+# print(readcsv('data/file0.csv'))   # expected output [['file0,line0'], ['file0,line1'], ['file0,line2'], ['file0,line3'], ['file0,line4'], ['file0,line5']]
