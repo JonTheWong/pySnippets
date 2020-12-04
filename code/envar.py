11ddb9dd34a770b3b655a02b070ec33fbbb1f01a
jonthewong <me@jonthewong.com>
2020-12-04 05:46:14 -0500

    forcing into global vars

--- code/envar.py
+++ code/envar.py
@@ -6,19 +6,24 @@ def envar():
     .env file required in project root
     touch ~.env
     git ignore .env
+    forcing global var
     '''
     if DEV = 0:
         '''Google Cloud Services''''
+        global GOOGLE_USER, GOOGLE_KEY, GOOGLE_DC
         GOOGLE_USER = config('API_GOOGLE_USER_ENV')
         GOOGLE_KEY = config('API_GOOGLE_KEY_ENV')
         GOOGLE_DC = config('API_GOOGLE_DC_ENV')
         '''WHMCS.COM'''
+        global WHMCS_USER, WHMCS_KEY
         WHMCS_USER = config('API_WHMCS_USER_ENV')
         WHMCS_KEY = config('API_WHMCS_KEY_ENV')
         '''WHM'''
+        global WHM_USER, WHM_KEY
         WHM_USER = config('API_WHM_USER_ENV')
         WHM_KEY = config('API_WHM_KEY_ENV')
         '''Amazon Web Services'''
+        global AMAZON_USER, AMAZON_KEY
         AMAZON_USER = config('AMAZON_USER_ENV')
         AMAZON_KEY = config('AMAZON_KEY_ENV')
 
